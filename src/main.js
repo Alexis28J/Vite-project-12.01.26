6f61be7e2ff766bd1d244a731183415301a15de2
MaNnu81 <mannuspamm@gmail.com>
2026-01-13 12:40:44 +0100

    Fine lezione (13.01.2026)

--- src/main.js
+++ src/main.js
@@ -1,42 +1,94 @@
 import "./style.css";
 import { WeatherService } from "./meteo-service.js";
+import { Chart, LineController, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
 
 const service = new WeatherService();
 
 service.getWeatherData()
-.then(weatherData => displayWeather(weatherData));
+    .then(weatherData => displayWeather(weatherData));
 
 
 function displayWeather(weatherData) {
 
-const container = document.getElementById('app');  //*
-container.innerHTML = "";  //*
+    const temperaturePoints = getTemperaturePoints(weatherData);
+    const rainPoints = getRainPoints(weatherData);
+    const windPoints = getWindPoints(weatherData);
 
-for (const data of weatherData) {   
-    
-const card = document.createElement('div');
+    testChart("temperature-chart", temperaturepoints);
+    testChart("rain-chart", rainpoints);
+    testChart("wind-chart", windpoints);
 
-const spanTime = document.createElement('span');
-spanTime.innerHTML = data.time;
-card.appendChild(spanTime);
+    const container = document.getElementById('app');  //*
+    container.innerHTML = "";  //*
 
-const spanRain = document.createElement('span');
-spanRain.innerHTML = data.rain;
-card.appendChild(spanRain);
+    for (const data of weatherData) {
 
-const spanTemp = document.createElement('span');
-spanTemp.innerHTML = data.temperature;
-card.appendChild(spanTemp);
+        const card = document.createElement('div');
 
-const spanCode = document.createElement('span');
-spanCode.innerHTML = data.code;
-card.appendChild(spanCode);
+        const spanTime = document.createElement('span');
+        spanTime.innerHTML = data.time;
+        card.appendChild(spanTime);
 
-const spanWind = document.createElement('span');
-spanWind.innerHTML = data.wind;
-card.appendChild(spanWind);
+        const spanRain = document.createElement('span');
+        spanRain.innerHTML = data.rain;
+        card.appendChild(spanRain);
 
-container.appendChild(card);
+        const spanTemp = document.createElement('span');
+        spanTemp.innerHTML = data.temperature;
+        card.appendChild(spanTemp);
+
+        const spanCode = document.createElement('span');
+        spanCode.innerHTML = data.code;
+        card.appendChild(spanCode);
+
+        const spanWind = document.createElement('span');
+        spanWind.innerHTML = data.wind;
+        card.appendChild(spanWind);
+
+        container.appendChild(card);
+    }
+
 }
 
+function getTemperaturePoints(meteoData) {
+    return [];
 }
+
+function getRainPoints(meteoData) {
+    return [];
+}
+
+function getWindPoints(meteoData) {
+    return [];
+}
+
+function testChart(canvasId, dataPoints) {  //*
+
+    //[{x:"2026-01-13T00:00", y:9},
+    //{x:"2026-01-13T01:00", y:8},
+    //{x:"2026-01-13T02:00", y:7},...]
+
+    Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement);
+
+    const labels = ["marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre"]
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+    };
+
+    const canvas = document.getElementById(canvasId);
+
+    new Chart(canvas, config)
+}
+
