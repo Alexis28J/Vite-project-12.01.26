7327dcaa70dea85187eb2ed920ae3a6fdfdf04db
MaNnu81 <mannuspamm@gmail.com>
2026-01-13 11:18:19 +0100

    Pausa lezione (13.01.2026)

--- src/main.js
+++ src/main.js
@@ -3,11 +3,40 @@ import { WeatherService } from "./meteo-service.js";
 
 const service = new WeatherService();
 
-service.getData()
-// .then(weatherData => displayWeather(weatherData));
+service.getWeatherData()
+.then(weatherData => displayWeather(weatherData));
 
-// function displayWeather (weatherData) {
 
-//         console.log('display', weatherData);
+function displayWeather(weatherData) {
 
-// }
+const container = document.getElementById('app');  //*
+container.innerHTML = "";  //*
+
+for (const data of weatherData) {   
+    
+const card = document.createElement('div');
+
+const spanTime = document.createElement('span');
+spanTime.innerHTML = data.time;
+card.appendChild(spanTime);
+
+const spanRain = document.createElement('span');
+spanRain.innerHTML = data.rain;
+card.appendChild(spanRain);
+
+const spanTemp = document.createElement('span');
+spanTemp.innerHTML = data.temperature;
+card.appendChild(spanTemp);
+
+const spanCode = document.createElement('span');
+spanCode.innerHTML = data.code;
+card.appendChild(spanCode);
+
+const spanWind = document.createElement('span');
+spanWind.innerHTML = data.wind;
+card.appendChild(spanWind);
+
+container.appendChild(card);
+}
+
+}
--- src/meteo-service.js
+++ src/meteo-service.js
@@ -1,39 +1,90 @@
-export class WeatherService {
+export class WeatherService {  //*
 
     static url = "https://api.open-meteo.com/v1/forecast?latitude=44.411&longitude=8.896&hourly=temperature_2m,rain,weather_code,wind_speed_10m";
 
-    constructor() {}
+    constructor() { }
 
-    getData (){
-        return fetch(WeatherService.url) 
-        .then(resp => resp.json())
-        .then(data => this.transformData(data))
-        .then(transformedData => console.log(transformedData));
-        
+    getWeatherData() {      //* 
+        return fetch(WeatherService.url)   //*
+            .then(resp => resp.json()) //*
+            .then(data => this.transformData(data)) //*
+            //.then(transformedData => console.log(transformedData));  //* 
+            .then(transformedData => transformedData);  //* 
     }
 
-   transformData(data){
-       fetch(WeatherService.url)
-       .then(resp => resp.json())
-       .then(data => {
-        const times = data.hourly.time;
-        const temperatures = data.hourly.temperature_2m;
-        const rains = data.hourly.rain;
-        const codes = data.hourly.weather_code;
-        const winds = data.hourly.wind_speed_10m;
+    transformData(data) {   //* 
 
-        const result = times.map((time, index) => ({
-            time, 
-            temperature: temperatures[index],
-            rain: rains[index],
-            code: codes[index],
-            wind: winds[index]
-        }));
+        //FATTO INSIEME A EVELYN IN CLASSE (12.01.2026)
+        //    fetch(WeatherService.url)
+        //    .then(resp => resp.json())
+        //    .then(data => {
+        //     const times = data.hourly.time;
+        //     const temperatures = data.hourly.temperature_2m;
+        //     const rains = data.hourly.rain;
+        //     const codes = data.hourly.weather_code;
+        //     const winds = data.hourly.wind_speed_10m;
 
-        console.log(result);
-        
-       })
-    
-   }
+        //     const result = times.map((time, index) => ({
+        //         time, 
+        //         temperature: temperatures[index],
+        //         rain: rains[index],
+        //         code: codes[index],
+        //         wind: winds[index]
+        //     }));
+
+        //     console.log(result);
+
+        //    })
+
+        //FATTO DAL PROF. IN CLASSE (13.01.2026)
+        console.log("Dati dell'API", data);
+
+        //*
+        const hourlyData = data.hourly;
+        //console.log("Solo i dati orari", hourlyData);   
+
+        const times = hourlyData.time;
+        //console.log("Orari", times);
+
+        const temperatures = hourlyData.temperature_2m;
+        //console.log("Temperature", temperatures);
+
+        const rains = hourlyData.rain;
+        //console.log("Rain", rains);
+
+        const codes = hourlyData.weather_code;
+        //console.log("Codes", codes);
+
+        const winds = hourlyData.wind_speed_10m;
+        //console.log("winds", winds);
+
+        const newArray = [];
+
+        for (let i = 0; i < times.length; i++) {   //usiamo times.length perché è l'array più lungo
+            const time = times[i];
+            const temperature = temperatures[i];
+            const rain = rains[i];
+            const code = codes[i];
+            const wind = winds[i];
+
+            // const hourData = {  //*
+            //     time: time,  
+            //     temperature: temperature,
+            //     rain: rain,
+            //     code: code,
+            //     wind: wind
+            // }
+
+            const hourData = {  //* va bene scriverlo anche in questo modo 
+                time,
+                temperature,
+                rain,
+                code,
+                wind
+            }
+            newArray.push(hourData);
+        }
+        return newArray;
+    }
 
 }
